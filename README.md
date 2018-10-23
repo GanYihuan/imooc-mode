282bdca3cbd0682440b68d8f40b6a953074b3a7a
GanYihuan <583520052@qq.com>
2018-10-23 17:56:31 +0800

    2-6 继承

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # imooc-mode
+
+## enviromenet
+
+> webpack
+
+```console
+npm install webpack webpack-cli webpack-dev-server html-webpack-plugin -D
+```
+
+## chap2
---
+++ chap2-OO/00-oo.js
@@ -0,0 +1,36 @@
+﻿/* father */
+class People {
+	constructor(name, age) {
+		this.name = name
+		this.age = age
+	}
+	eat() {
+		alert(`${this.name} eat something`)
+	}
+	speak() {
+		alert(`My name is ${this.name}, age ${this.age}`)
+	}
+}
+
+/* child */
+class Student extends People {
+	constructor(name, age, number) {
+    /* super(): invoked father constructor */
+		super(name, age)
+		this.number = number
+	}
+	study() {
+		alert(`${this.name} study`)
+	}
+}
+
+let zhang = new People('zhang', 20)
+zhang.eat()
+zhang.speak()
+
+let wang = new People('wang', 21)
+wang.eat()
+wang.speak()
+
+let xiaohong = new Student('xiaohong', 11, 'A2')
+xiaohong.study()

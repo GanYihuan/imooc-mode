597e19c6024435079524f9fdb247b15e8b402eb1
GanYihuan <583520052@qq.com>
2018-10-23 18:06:21 +0800

    2-8 多态

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ npm install webpack webpack-cli webpack-dev-server html-webpack-plugin -D
 ```
 
 ## chap2
+
+- public 完全开放
+- protected 对子类开放
+- private 对自己开放
+- `_` 开头是 private
--- chap2-OO/00-oo.js
+++ chap2-OO/00-oo.js
@@ -10,27 +10,44 @@ class People {
 	speak() {
 		alert(`My name is ${this.name}, age ${this.age}`)
 	}
+	saySomething() {}
 }
 
 /* child */
 class Student extends People {
 	constructor(name, age, number) {
-    /* super(): invoked father constructor */
+		/* super(): invoked father constructor */
 		super(name, age)
 		this.number = number
 	}
 	study() {
 		alert(`${this.name} study`)
 	}
+	saySomething() {
+		alert('I am Student')
+	}
 }
 
+class Teacher extends People {
+	constructor(name) {
+		super(name)
+	}
+	saySomething() {
+		alert('I am Teacher')
+	}
+}
+
+/* OO */
 let zhang = new People('zhang', 20)
 zhang.eat()
 zhang.speak()
 
-let wang = new People('wang', 21)
-wang.eat()
-wang.speak()
-
+/* extend */
 let xiaohong = new Student('xiaohong', 11, 'A2')
 xiaohong.study()
+
+/* polymorphic */
+let a = new A('a')
+a.saySomething()
+let b = new B('b')
+b.saySomething()
